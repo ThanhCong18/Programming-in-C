fd7c9b669970677f9764f8a2787c836fd83a4580
ThanhCong18 <thanhcong200500@gmail.com>
2023-10-27 06:34:25 +0700

    so lan xuat hien

---
+++ source/so_lan_xuat_hien_trong_mang.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int array[10] , n ;
+    printf("Nhap so phan tu cua Array");
+    scanf("%d" , &n);
+    for(int i = 0 ; i < n ; i++)
+    {
+        scanf("%d" , &array[i]);
+    }
+    // kiểm tra với phần tử trước nó
+    for(int i = 0 ; i < n ; i++)
+    {
+       int OK = 1 ;
+       for( int j = 0 ; j < i ; j++)
+       {
+           if(array[i]==array[j])
+           {
+               OK = 0 ; break ;
+           }
+       }
+       if(OK) printf("%d " , array[i]);
+    }
+    return 0;
+}
